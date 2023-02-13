f058905644ab3760b6546074f0bc9b43c87adefc
Minigrim0 <grimauflorent@gmail.com>
2023-02-13 18:03:14 +0100

    [Upd] Moved script folder to root + merge spawners

--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -1,17 +1,19 @@
 extends Node2D
 
-onready var car = preload("res://assets/prefabs/Car.tscn")
+export(String, FILE, "*.tscn") var object_to_spawn
+export(int) var spawn_pixel_cooldown = 2000 # pixels
+export(int, "Random", "Sine", "Not defined") var spawn_type
 
-export(int) var spawn_cooldown = 2000 # pixels
-
+var object  # The object to spawn
 var meters_at_last_spawn
 
 func _ready():
+	object = load(object_to_spawn)
 	meters_at_last_spawn = global_position.x
 
 func _process(_delta):
 	# Updates the position of the spawner, to determine whether to spawn a trashcan or not
-	if global_position.x - meters_at_last_spawn >= spawn_cooldown:
+	if global_position.x - meters_at_last_spawn >= spawn_pixel_cooldown:
 		spawn()
 		meters_at_last_spawn = global_position.x
 
@@ -19,6 +21,6 @@ func spawn():
 	# Changes the position in y of the spawner randomly within the correct area)
 	# And spawns a trashcan as child of the root component
 	global_position.y = 550 + rand_range(-200, 200)
-	var carInstance = car.instance()
-	carInstance.position  = global_position
-	get_tree().get_root().add_child(carInstance)
+	var objInstance = object.instance()
+	objInstance.position  = global_position
+	get_tree().get_root().add_child(objInstance)
