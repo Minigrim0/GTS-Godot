374b8f9f88932eb0ba91820797f2e36ea9ad489d
Minigrim0 <grimauflorent@gmail.com>
2023-02-13 18:31:19 +0100

    [Upd] Added lower & upper bounds for the spawner

--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -3,13 +3,17 @@ extends Node2D
 export(String, FILE, "*.tscn") var object_to_spawn
 export(int) var spawn_pixel_cooldown = 2000 # pixels
 export(int, "Random", "Sine", "Not defined") var spawn_type
+export(int) var upper_bound = 750
+export(int) var lower_bound = 375
 
 var object  # The object to spawn
 var meters_at_last_spawn
 
+
 func _ready():
 	object = load(object_to_spawn)
 	meters_at_last_spawn = global_position.x
+
 
 func _process(_delta):
 	# Updates the position of the spawner, to determine whether to spawn a trashcan or not
@@ -17,10 +21,11 @@ func _process(_delta):
 		spawn()
 		meters_at_last_spawn = global_position.x
 
+
 func spawn():
 	# Changes the position in y of the spawner randomly within the correct area)
 	# And spawns a trashcan as child of the root component
-	global_position.y = 550 + rand_range(-200, 200)
+	global_position.y = rand_range(lower_bound, upper_bound)
 	var objInstance = object.instance()
 	objInstance.position  = global_position
 	get_tree().get_root().add_child(objInstance)
