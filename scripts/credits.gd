4153655586095b5a6dfed0678355241889e4104f
Minigrim0 <grimauflorent@gmail.com>
2023-02-19 23:03:36 +0100

    [Upd] Credits script to load from json

--- scripts/credits.gd
+++ scripts/credits.gd
@@ -13,73 +13,39 @@ onready var line := $CreditsContainer/Line
 var started := false
 var finished := false
 
-var section
 var section_next := true
 var section_timer := 0.0
-var line_timer := 0.0
-var curr_line := 0
 var lines := []
 
-var credits = [
-	[
-		"A game by Awesome Game Company"
-	],[
-		"Programming",
-		"Programmer Name",
-		"Programmer Name 2"
-	],[
-		"Art",
-		"Artist Name"
-	],[
-		"Music",
-		"Musician Name"
-	],[
-		"Sound Effects",
-		"SFX Name"
-	],[
-		"Testers",
-		"Name 1",
-		"Name 2",
-		"Name 3"
-	],[
-		"Tools used",
-		"Developed with Godot Engine",
-		"https://godotengine.org/license",
-		"",
-		"Art created with My Favourite Art Program",
-		"https://myfavouriteartprogram.com"
-	],[
-		"Special thanks",
-		"My parents",
-		"My friends",
-		"My pet rabbit"
-	]
-]
+export(String, FILE, "*.json") var credits_file
+var credits
+
+func load_credits():
+	var file = File.new()
+	file.open(credits_file, File.READ)
+	credits = JSON.parse(file.get_as_text()).result
+	file.close()
+
+
+func _ready():
+	load_credits()
 
 
 func _process(delta):
 	var scroll_speed = base_speed * delta
-	
-	if section_next:
-		section_timer += delta * speed_up_multiplier if speed_up else delta
-		if section_timer >= section_time:
+
+	if section_next:  # If a new section is starting (Or must start)
+		section_timer += delta * speed_up_multiplier if speed_up else delta  # Raise the timer
+		if section_timer >= section_time:  # If the timer is above the threshold
 			section_timer -= section_time
-			
-			if credits.size() > 0:
-				started = true
-				section = credits.pop_front()
-				curr_line = 0
-				add_line()
-	
-	else:
-		line_timer += delta * speed_up_multiplier if speed_up else delta
-		if line_timer >= line_time:
-			line_timer -= line_time
-			add_line()
-	
+
+			if credits.size() > 0:  # If there is still something in the credits
+				var section = credits.pop_front()  # Take the first element
+				add_section(section)  # Add a line
+
 	if speed_up:
 		scroll_speed *= speed_up_multiplier
-	
+
 	if lines.size() > 0:
 		for l in lines:
 			l.rect_position.y -= scroll_speed
@@ -93,25 +59,23 @@ func _process(delta):
 func finish():
 	if not finished:
 		finished = true
-		# NOTE: This is called when the credits finish
-		# - Hook up your code to return to the relevant scene here, eg...
-		#get_tree().change_scene("res://scenes/MainMenu.tscn")
+		get_tree().change_scene("res://scenes/MainMenu.tscn")
 
 
-func add_line():
-	var new_line = line.duplicate()
-	new_line.text = section.pop_front()
-	lines.append(new_line)
-	if curr_line == 0:
-		new_line.add_color_override("font_color", title_color)
-	$CreditsContainer.add_child(new_line)
-	
-	if section.size() > 0:
-		curr_line += 1
-		section_next = false
-	else:
-		section_next = true
+func add_section(section):
+	var section_title = section["name"]
+	var title_line = line.duplicate()
+	title_line.text = section_title
+	title_line.add_color_override("font_color", title_color)
+	$CreditsContainer.add_child(title_line)
+	lines.append(title_line)
 
+	for index in section["values"].size():
+		var value_line = line.duplicate()
+		value_line.text = section["values"][index]
+		value_line.rect_position.y += (index + 2) * (value_line.rect_size.y + 5)
+		$CreditsContainer.add_child(value_line)
+		lines.append(value_line)
 
 func _unhandled_input(event):
 	if event.is_action_pressed("ui_cancel"):
