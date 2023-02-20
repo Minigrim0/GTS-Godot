f5d3cc635593146ac7f9164fd75c2878ae29f623
Minigrim0 <grimauflorent@gmail.com>
2023-02-20 18:34:19 +0100

    [Upd] use the returned value of change_scene

--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -4,10 +4,10 @@ func _on_Quit_Button_pressed():
 	get_tree().quit(0)
 
 func _on_Credits_Button_pressed():
-	get_tree().change_scene("res://scenes/credits.tscn")
+	assert(get_tree().change_scene("res://scenes/credits.tscn") == 0, "Error while switching to scene credits")
 
 func _on_Options_Button_pressed():
 	pass # Replace with function body.
 
 func _on_NewGame_Button_pressed():
-	get_tree().change_scene("res://scenes/TestLevel.tscn")
+	assert(get_tree().change_scene("res://scenes/TestLevel.tscn") == 0, "Error while loading level")
