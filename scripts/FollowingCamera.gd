b63dea417d8f4ffb7dc6a0f567cf5951abfd2d25
Minigrim0 <grimauflorent@gmail.com>
2023-02-22 15:23:25 +0100

    [Add] Distance left + moved UI in its own scene

--- scripts/FollowingCamera.gd
+++ scripts/FollowingCamera.gd
@@ -1,12 +1,35 @@
 extends Node2D
 
-export(NodePath) var followed_item
+export(NodePath) var followedItemPath
+export(NodePath) var distanceLeftLabelPath
+export(NodePath) var endOfLevelPath
+
+var followedItem
+var distanceLeftLabel
+var endOfLevel
+
+
+func _ready():
+	if distanceLeftLabelPath != null:
+		distanceLeftLabel = get_node(distanceLeftLabelPath)
+	if endOfLevelPath != null:
+		endOfLevel = get_node(endOfLevelPath)
+	if followedItemPath != null:
+		followedItem = get_node(followedItemPath)
+
 
 func _process(_delta):
-	if followed_item != null:
-		position.x = get_node(followed_item).position.x
+	if followedItem != null:
+		position.x = followedItem.position.x
+	updDistanceLeft()
 
 
 func _on_LevelEnd_area_entered(area):
 	if area.name == "EndCollisionCheck":
-		followed_item = null
+		followedItem = null
+
+
+func updDistanceLeft():
+	if endOfLevel != null:
+		var distance = (endOfLevel.position.x - position.x) / 100
+		distanceLeftLabel.text = "%d meters" % distance
