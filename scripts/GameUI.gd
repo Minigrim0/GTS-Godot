83dd33051742e35e856d366520b01fb5f474242a
Minigrim0 <grimauflorent@gmail.com>
2023-03-02 14:01:29 +0100

    [Upd] Renamed FollowingCamera -> GameUI + added signal for truck leaving scene

--- scripts/GameUI.gd
+++ scripts/GameUI.gd
@@ -1,17 +1,13 @@
 extends Node2D
 
 export(NodePath) var followedItemPath
-export(NodePath) var distanceLeftLabelPath
 export(NodePath) var endOfLevelPath
 
 var followedItem
-var distanceLeftLabel
 var endOfLevel
 
 
 func _ready():
-	if distanceLeftLabelPath != null:
-		distanceLeftLabel = get_node(distanceLeftLabelPath)
 	if endOfLevelPath != null:
 		endOfLevel = get_node(endOfLevelPath)
 	if followedItemPath != null:
@@ -28,7 +24,10 @@ func _on_LevelEnd_area_entered(area):
 	if area.name == "EndCollisionCheck":
 		followedItem = null
 
+func _on_TruckEndCollisionCheck_body_entered(body):
+	if body.name == "Truck":
+		print("End of Level")
 
 func updDistanceLeft():
 	if endOfLevel != null:
-		distanceLeftLabel.text = "%d meters" % ((endOfLevel.position.x - position.x) / 100)
+		$MarginContainer/VBoxContainer/HBoxContainer/Informations/VBoxContainer/DistanceLeft.text = "%d meters" % ((endOfLevel.position.x - position.x) / 100)
