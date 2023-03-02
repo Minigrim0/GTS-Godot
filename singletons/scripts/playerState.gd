98b0ba69be041286b0e4b1b5a93b215775cc4da2
Minigrim0 <grimauflorent@gmail.com>
2023-03-02 17:17:46 +0100

    [Upd] singletons location

---
+++ singletons/scripts/playerState.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+var euros
+var current_health = 100
+
+func load_save_file(path):
+	pass
+
+func save(save_name):
+	pass
+
+func get_money():
+	return euros
+
+func add_money(to_add):
+	euros += to_add
