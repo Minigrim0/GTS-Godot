c7164ac36029f2fdf9a79efb320571a308b2d3b4
Minigrim0 <grimauflorent@gmail.com>
2023-03-05 13:50:33 +0100

    [Upd] Have elements communicate with playerState

--- scripts/GameUI.gd
+++ scripts/GameUI.gd
@@ -2,32 +2,58 @@ extends Node2D
 
 export(NodePath) var followedItemPath
 export(NodePath) var endOfLevelPath
+export(NodePath) var truckPath
 
 var followedItem
 var endOfLevel
+var truck
 
 
 func _ready():
+	$"/root/PlayerState".level_start()
 	if endOfLevelPath != null:
 		endOfLevel = get_node(endOfLevelPath)
 	if followedItemPath != null:
 		followedItem = get_node(followedItemPath)
+	if truckPath != null:
+		truck = get_node(truckPath)
 
 
 func _process(_delta):
 	if followedItem != null:
 		position.x = followedItem.position.x
-	updDistanceLeft()
+	updateGauges()
+	updateTextElements()
 
 
 func _on_LevelEnd_area_entered(area):
 	if area.name == "EndCollisionCheck":
 		followedItem = null
 
+
 func _on_TruckEndCollisionCheck_body_entered(body):
 	if body.name == "Truck":
-		print("End of Level")
+		$"/root/PlayerState".level_end()
+		assert(get_tree().change_scene("res://scenes/RecapScene.tscn") == 0, "Error while loading the recap")
 
-func updDistanceLeft():
+
+func updateGauges():
+	var nitro_level = truck.get_nitro_level()
+	$MarginContainer/VBoxContainer/HBoxContainer/NitroGauge/TextureProgress.value = nitro_level
+	$MarginContainer/VBoxContainer/HBoxContainer/NitroGauge/NitroPercent.text = "%d%%" % nitro_level
+
+	var speed = truck.get_speed()
+	$MarginContainer/VBoxContainer/HBoxContainer/SpeedGauge/TextureProgress.value = speed
+	$MarginContainer/VBoxContainer/HBoxContainer/SpeedGauge/SpeedText.text = "%d km/h" % speed
+
+	var alcohol_level = truck.get_alcohol_level()
+	$MarginContainer/VBoxContainer/HBoxContainer/AlcoholGauge/TextureProgress.value = alcohol_level
+	$MarginContainer/VBoxContainer/HBoxContainer/AlcoholGauge/AlcoholText.text = "%f%%" % alcohol_level
+
+
+func updateTextElements():
 	if endOfLevel != null:
-		$MarginContainer/VBoxContainer/HBoxContainer/Informations/VBoxContainer/DistanceLeft.text = "%d meters" % ((endOfLevel.position.x - position.x) / 100)
+		$MarginContainer/VBoxContainer/HBoxContainer/Informations/ValuesVBoxContainer/DistanceLeft.text = "%d meters" % ((endOfLevel.position.x - position.x) / 100)
+	$MarginContainer/VBoxContainer/HBoxContainer/Informations/ValuesVBoxContainer/TopSpeed.text = str($"/root/PlayerState".get_top_speed())
+	$MarginContainer/VBoxContainer/HBoxContainer/Informations/ValuesVBoxContainer/Income.text = str($"/root/PlayerState".get_score())
+	$MarginContainer/VBoxContainer/HBoxContainer/Informations/ValuesVBoxContainer/Efficiency.text = str($"/root/PlayerState".get_efficiency("trash"))
--- scripts/TrashCan.gd
+++ scripts/TrashCan.gd
@@ -5,6 +5,7 @@ var velocity = Vector2.ZERO
 func _ready():
 	$ExplosionSprite.hide()
 	velocity = Vector2.ZERO
+	$"/root/PlayerState".add_spawned_item("trash", 1)
 
 func explode():
 	$AnimationPlayer.current_animation = "Explode"
@@ -17,4 +18,5 @@ func die():
 func _on_TrashCan_body_entered(body):
 	if body.name == "Truck":
 		body.trashCollected()
+		$"/root/PlayerState".add_collected_item("trash", 1)
 		explode()
